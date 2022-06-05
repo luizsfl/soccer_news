39acece524d1b493c8cfa9f1ad1745055166bf63
Pedro Luiz <luizsfl@hotmail.com>
2022-06-05 03:50:04 -0300

    Implementando o shiperrefresh

--- app/src/main/java/pedroluiz/projeto/soccernews/ui/news/NewsFragment.kt
+++ app/src/main/java/pedroluiz/projeto/soccernews/ui/news/NewsFragment.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import pedroluiz.projeto.soccernews.databinding.FragmentNewsBinding
 import pedroluiz.projeto.soccernews.ui.adapter.NewsAdapter
 
 
 class NewsFragment : Fragment() {
 
+    private lateinit var newsViewModel: NewsViewModel
     private var _binding: FragmentNewsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,7 +26,7 @@ class NewsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val newsViewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
+        newsViewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
         newsViewModel.NewsViewModel()
 
         _binding = FragmentNewsBinding.inflate(inflater, container, false)
@@ -32,23 +34,38 @@ class NewsFragment : Fragment() {
 
         binding.rcNews.layoutManager = LinearLayoutManager(context)
 
+        observNews()
 
-        newsViewModel.listNews.observe(viewLifecycleOwner) {
-            binding.rcNews.adapter = NewsAdapter(it, NewsAdapter.NewsFavoriteListener{
-               newsViewModel.saveNews(it)
-            })
-        }
+        observeStates()
 
-        newsViewModel.getState.observe(viewLifecycleOwner) {
-            when(it){
-                NewsViewModel.State.DOING -> print("todo incluis INICIAR wsiperefreshlayout LOADING")
-                NewsViewModel.State.DONE -> print("todo incluis FINALIZAR wsiperefreshlayout LOADING")
-                NewsViewModel.State.ERROR -> print("todo incluis FINALIZAR wsiperefreshlayout LOADING") //TODO MOSTRAR um erro generico
-            }
-
+        binding.srfNews.setOnRefreshListener {
+            newsViewModel.findNews()
         }
 
         return root
+    }
+
+    private fun observeStates() {
+        newsViewModel.getState.observe(viewLifecycleOwner) {
+            when (it) {
+                NewsViewModel.State.DOING -> binding.srfNews.isRefreshing = true
+                NewsViewModel.State.DONE -> binding.srfNews.isRefreshing = false
+                NewsViewModel.State.ERROR -> {
+                    binding.srfNews.isRefreshing = false
+                    Snackbar.make(binding.srfNews, "Network error", Snackbar.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+        }
+    }
+
+    private fun observNews() {
+        newsViewModel.listNews.observe(viewLifecycleOwner) {
+            binding.rcNews.adapter = NewsAdapter(it, NewsAdapter.NewsFavoriteListener {
+                newsViewModel.saveNews(it)
+            })
+        }
     }
 
     override fun onDestroyView() {
